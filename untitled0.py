644ed9bdb4210235beb717084b3617fc3c03788d
herbz11p <114390428+herbz11p@users.noreply.github.com>
2022-09-26 14:56:26 +0300

    Update untitled0.py

--- untitled0.py
+++ untitled0.py
@@ -13,7 +13,7 @@ import plotly.figure_factory as ff
 
 
 st.title("GDP on India")
-GDP_Of_India=pd.read_csv("GDP Growth of India - Sheet1 (1).csv")
+GDP_Of_India=pd.read_csv("large_df (2).csv")
 
 
 gdp_india=px.scatter(GDP_Of_India, x="Year", y= "GDP of IND")
